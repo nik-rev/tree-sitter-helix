33786948fb803804e9d7c516cbba947533d8dce5
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-01-27 19:26:24 +0000

    feat: text is not private

--- grammar.js
+++ grammar.js
@@ -14,12 +14,12 @@ module.exports = grammar({
     source_file: ($) => repeat($._definition),
 
     _definition: ($) =>
-      choice($.primary_left, $.primary_right, $.left, $.right, $._text),
+      choice($.primary_left, $.primary_right, $.left, $.right, $.text),
 
-    // TODO: figure out how to allow more characters as _text.
+    // TODO: figure out how to allow more characters as text.
     // Ideally we would allow anything other than our keyword characters,
     // but I'm not sure how to do that
-    _text: () => /[\d\w\s]+/,
+    text: () => /[\d\w\s]+/,
 
     _outer_text_token: () => /[^#]/,
 
@@ -27,22 +27,22 @@ module.exports = grammar({
     _closing_primary_left: () => "]#",
 
     primary_left: ($) =>
-      seq($._opening_primary_left, $._text, $._closing_primary_left),
+      seq($._opening_primary_left, $.text, $._closing_primary_left),
 
     _opening_primary_right: () => "#[",
     _closing_primary_right: () => "|]#",
 
     primary_right: ($) =>
-      seq($._opening_primary_right, $._text, $._closing_primary_right),
+      seq($._opening_primary_right, $.text, $._closing_primary_right),
 
     _opening_left: () => "#(|",
     _closing_left: () => ")#",
 
-    left: ($) => seq($._opening_left, $._text, $._closing_left),
+    left: ($) => seq($._opening_left, $.text, $._closing_left),
 
     _opening_right: () => "#(",
     _closing_right: () => "|)#",
 
-    right: ($) => seq($._opening_right, $._text, $._closing_right),
+    right: ($) => seq($._opening_right, $.text, $._closing_right),
   },
 });
